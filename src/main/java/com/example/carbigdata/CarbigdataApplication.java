aeb261704bceee10c794ac84d996c0edfc22204d
Jhuan Pablo <jhuanpablo@MacBook-Air-de-Jhuan.local>
2025-03-14 16:40:29 -0300

    criando ms

---
+++ src/main/java/com/example/carbigdata/CarbigdataApplication.java
@@ -0,0 +1,13 @@
+package com.example.carbigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbigdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbigdataApplication.class, args);
+	}
+
+}
